5b4ba7fa4e4f960900c33204a35eb81b50610cda
Arun Gupta <arun.gupta@gmail.com>
2019-06-27 06:17:36 -0700

    trying Prometheus-style metrics using actuator

--- app/src/main/java/org/aws/samples/greeting/GreetingController.java
+++ app/src/main/java/org/aws/samples/greeting/GreetingController.java
@@ -2,6 +2,8 @@ package org.aws.samples.greeting;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 
 /**
  * @author Arun Gupta
@@ -9,8 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class GreetingController {
 
+    // request counter metric
+    static final Counter REQUESTS = Counter.build()
+            .name("requests_total").help("Total number of requests.").register();
+    // histogram metric
+    static final Histogram REQUESTS_LATENCY = Histogram.build()
+            .name("requests_latency_seconds").help("Request latency in seconds.").register();
+
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hello";
+        REQUESTS.inc();
+        Histogram.Timer requestTimer = REQUESTS_LATENCY.startTimer();
+        try {
+            return "Hello World";
+        } finally {
+            requestTimer.observeDuration();
+        }
     }
 }
